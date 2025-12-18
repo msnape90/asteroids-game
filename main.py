f72f13379b8a83c7e134507c69499a5cbf93d0af
msnape90 <msnape90@gmail.com>
2025-12-18 14:04:00 -0500

    added dt var and clock tick

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -23,6 +25,8 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
